9dc25da3a78e945140eb5febe37b00ba8e540f3b
Thays Ribeiro <thaysribeiros@gmail.com>
2021-03-18 21:46:24 -0700

    Create Ex07 - 	my_string_formatting.c
    Created a my_string_formatting function that takes 3 parameters (firstname, lastname, and age) and prints a string composed value.

---
+++ Quest02/Ex07 - 	my_string_formatting.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void my_string_formatting(char* first, char* last, int age)
+{
+	printf("Hello, my name is %s %s, I'm %d.\n", first, last, age);
+}
+/*
+char main()
+{
+	my_string_formatting("Baby", "Yoda", 50);
+	my_string_formatting("Marie", "Curie", 26);
+	my_string_formatting("john", "doe", 37);
+}
+*/
