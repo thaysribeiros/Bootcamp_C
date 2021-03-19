fb344f3a89677eefe5ae8cb63c9e2a61d30dd82f
Thays Ribeiro <thaysribeiros@gmail.com>
2021-03-18 20:46:21 -0700

    Create Ex05 - my_first_incrementation.c
    Incrementation and decrementation depending of the language it's either ++ (--) or += 1 (-= 1).

---
+++ Quest01/Ex05 - my_first_incrementation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	int my_index;
+  	my_index = 0;
+
+    my_index += 1;
+  	printf("%d\n", my_index);
+  	my_index -= 1;
+  	printf("%d\n", my_index);
+  	my_index += 2;
+  	printf("%d\n", my_index);
+  return 0;
+}
