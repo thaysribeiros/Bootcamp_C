64159267e62d557e3a50b1568abf91f6c79aa720
Thays Ribeiro <thaysribeiros@gmail.com>
2021-03-18 21:42:29 -0700

    Create Ex04 - my_add.c
    Created a my_add function that takes 2 parameters (nbr1 and nbr2) and returns a value. This value is the result of the addition of nbr1 and nbr2 parameters.

---
+++ Quest02/Ex04 - my_add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int my_add(int nbr1, int nbr2)
+{
+	int total;
+	total = nbr1 + nbr2;
+
+	return total;
+
+}
+/*
+int main(){
+	printf("%d\n", my_add(1, 2));
+	printf("%d\n", my_add(10,10));
+	printf("%d\n", my_add(-10, 10));
+}*/
