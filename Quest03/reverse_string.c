9221500d35e805536d2704990bbfcacf3437a17a
Thays Ribeiro <thaysribeiros@gmail.com>
2021-03-18 22:17:05 -0700

    Create reverse_string.c
    Write a program that takes a string as an argument returns its reverses. An in-place algorithm is an algorithm that transforms input using no auxiliary data structure.

---
+++ Quest03/reverse_string.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int my_strlen(char* str)
+{
+    int len = 0;
+    while (str[len] != '\0')
+    {
+        len += 1;
+    }
+    return len;
+}
+char* reverse_string(char* str)
+{
+    static char revstr[100];
+	int len = my_strlen(str);
+    int index = len - 1;
+    int j = 0;
+    
+    while (index >= 0)
+    {
+        revstr[j] = str[index];
+        j += 1;
+        index -= 1;
+    }
+    revstr[len] = '\0';
+    return revstr;
+}
+/*
+int main() {
+
+    char* str = "baby yoda";
+	
+	printf("Before reverse -> %s\n", str);
+  	printf("Reverse -> %s\n", reverse_string(str));
+	return 0;
+	}*/
