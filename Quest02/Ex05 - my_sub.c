0a4e90f90a64007f350cfcfa7cdfc1926770184f
Thays Ribeiro <thaysribeiros@gmail.com>
2021-03-18 21:43:45 -0700

    Create Ex05 - my_sub.c
    Created a my_sub function that takes 2 parameters (nbr1 and nbr2) and returns a value. This value is the result of the subtraction of nbr1 and nbr2 parameters.

---
+++ Quest02/Ex05 - my_sub.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int my_sub(int nbr1, int nbr2)
+{
+        int total;
+        total = nbr1 - nbr2;
+        return total;
+}
+/*
+int main(){
+        printf("%d\n", my_sub(1, 2));
+        printf("%d\n", my_sub(10,10));
+        printf("%d\n", my_sub(-10, 10));
+}*/     
