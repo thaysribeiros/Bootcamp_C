4501fc703f83aa38d2a97a791c7714e4bdf72fe8
Thays Ribeiro <thaysribeiros@gmail.com>
2021-03-18 21:44:51 -0700

    Create Ex06 - my_mult.c
    Created a my_mult function that takes 2 parameters (nbr1 and nbr2) and returns a value. This value is the result of the multiplication of nbr1 and nbr2 parameters.

---
+++ Quest02/Ex06 - my_mult.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int my_mult(int nbr1, int nbr2)
+{
+        int total;
+        total = nbr1 * nbr2;
+        return total;
+}
+/*
+int main(){
+        printf("%d\n", my_mult(1, 2));
+        printf("%d\n", my_mult(10,10));
+        printf("%d\n", my_mult(-10, 10));
+}*/
