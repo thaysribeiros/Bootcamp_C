be483e62a450e838855a83dc4b8f7fa49341e771
Thays Ribeiro <thaysribeiros@gmail.com>
2021-03-19 00:18:21 -0700

    Create my_define.c

---
+++ Quest07/my_define.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+
+#define EVEN(N) N%2 == 0
+//#XXXX SUCCESS XXXXXX
+#define EVEN_MSG "I have an even number of arguments."
+#define ODD_MSG "I have an odd number of arguments."
+typedef enum s_bool
+{
+    TRUE,
+    FALSE,
+} t_bool;
+
+void	my_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void	my_putstr(char *str)
+{
+    int index;
+    
+    index = 0;
+  	while (str[index] != '\0') {
+      my_putchar(str[index]);
+  	 	index++;
+  	}
+}
+
+t_bool	my_is_even(int nbr)
+{
+	return ((EVEN(nbr)) ? TRUE : FALSE);
+}
+
+void my_define(int argc)
+{
+	if (my_is_even(argc) == TRUE) {
+    my_putstr(EVEN_MSG);
+    my_putchar('\n');
+  }
+	else {
+    my_putstr(ODD_MSG);
+    my_putchar('\n');
+  }
+}
+/*
+int main()
+{
+    char str = '2';
+    my_define(str);
+    return 0;
+}*/
